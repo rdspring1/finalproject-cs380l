4b9f835dd7dc4d2c4c4b6c03b74fe598e0c04008
RyanDS <rdspring1@gmail.com>
2013-11-18 05:52:47 -0600

    Added Support for Data Transfers

--- pen_register.c
+++ pen_register.c
@@ -1,42 +1,98 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/usb.h>
-
+ 
+#define MIN(a,b) (((a) <= (b)) ? (a) : (b))
+#define BULK_EP_OUT 0x01
+#define BULK_EP_IN 0x82
+#define MAX_PKT_SIZE 512
+ 
 static struct usb_device *device;
-
-static int pen_probe(struct usb_interface *interface, const struct usb_device_id *id)
+static struct usb_class_driver class;
+static unsigned char bulk_buf[MAX_PKT_SIZE];
+ 
+static int pen_open(struct inode *i, struct file *f)
 {
-    struct usb_host_interface *iface_desc;
-    struct usb_endpoint_descriptor *endpoint;
-    int i;
+    return 0;
+}
+static int pen_close(struct inode *i, struct file *f)
+{
+    return 0;
+}
+static ssize_t pen_read(struct file *f, char __user *buf, size_t cnt, loff_t *off)
+{
+    int retval;
+    int read_cnt;
  
-    iface_desc = interface->cur_altsetting;
-    printk(KERN_INFO "Pen i/f %d now probed: (%04X:%04X)\n",
-            iface_desc->desc.bInterfaceNumber, id->idVendor, id->idProduct);
-    printk(KERN_INFO "ID->bNumEndpoints: %02X\n",
-            iface_desc->desc.bNumEndpoints);
-    printk(KERN_INFO "ID->bInterfaceClass: %02X\n",
-            iface_desc->desc.bInterfaceClass);
- 
-    for (i = 0; i < iface_desc->desc.bNumEndpoints; i++)
+    /* Read the data from the bulk endpoint */
+    retval = usb_bulk_msg(device, usb_rcvbulkpipe(device, BULK_EP_IN),
+            bulk_buf, MAX_PKT_SIZE, &read_cnt, 5000);
+    if (retval)
     {
-        endpoint = &iface_desc->endpoint[i].desc;
- 
-        printk(KERN_INFO "ED[%d]->bEndpointAddress: 0x%02X\n",
-                i, endpoint->bEndpointAddress);
-        printk(KERN_INFO "ED[%d]->bmAttributes: 0x%02X\n",
-                i, endpoint->bmAttributes);
-        printk(KERN_INFO "ED[%d]->wMaxPacketSize: 0x%04X (%d)\n",
-                i, endpoint->wMaxPacketSize, endpoint->wMaxPacketSize);
+        printk(KERN_ERR "Bulk message returned %d\n", retval);
+        return retval;
+    }
+    if (copy_to_user(buf, bulk_buf, MIN(cnt, read_cnt)))
+    {
+        return -EFAULT;
     }
  
+    return MIN(cnt, read_cnt);
+}
+static ssize_t pen_write(struct file *f, const char __user *buf, size_t cnt, loff_t *off)
+{
+    int retval;
+    int wrote_cnt = MIN(cnt, MAX_PKT_SIZE);
+ 
+    if (copy_from_user(bulk_buf, buf, MIN(cnt, MAX_PKT_SIZE)))
+    {
+        return -EFAULT;
+    }
+ 
+    /* Write the data into the bulk endpoint */
+    retval = usb_bulk_msg(device, usb_sndbulkpipe(device, BULK_EP_OUT),
+            bulk_buf, MIN(cnt, MAX_PKT_SIZE), &wrote_cnt, 5000);
+    if (retval)
+    {
+        printk(KERN_ERR "Bulk message returned %d\n", retval);
+        return retval;
+    }
+ 
+    return wrote_cnt;
+}
+ 
+static struct file_operations fops =
+{
+    .open = pen_open,
+    .release = pen_close,
+    .read = pen_read,
+    .write = pen_write,
+};
+ 
+static int pen_probe(struct usb_interface *interface, const struct usb_device_id *id)
+{
+    int retval;
+ 
     device = interface_to_usbdev(interface);
-    return 0;
+ 
+    class.name = "usb/pen%d";
+    class.fops = &fops;
+    if ((retval = usb_register_dev(interface, &class)) < 0)
+    {
+        /* Something prevented us from registering this driver */
+        err("Minor registering failed");
+    }
+    else
+    {
+        printk(KERN_INFO "Minor obtained: %d\n", interface->minor);
+    }
+ 
+    return retval;
 }
  
 static void pen_disconnect(struct usb_interface *interface)
 {
-    printk(KERN_INFO "Pen drive removed\n");
+    usb_deregister_dev(interface, &class);
 }
  
 static struct usb_device_id pen_table[] =
@@ -50,18 +106,26 @@ MODULE_DEVICE_TABLE (usb, pen_table);
 static struct usb_driver pen_driver =
 {
     .name = "pen_driver",
-    .id_table = pen_table,
     .probe = pen_probe,
     .disconnect = pen_disconnect,
+    .id_table = pen_table,
 };
  
 static int __init pen_init(void)
 {
-    return usb_register(&pen_driver);
+    int result;
+ 
+    /* Register this driver with the USB subsystem */
+    if ((result = usb_register(&pen_driver)))
+    {
+        err("Failed to register USB driver. Error number %d", result);
+    }
+    return result;
 }
  
 static void __exit pen_exit(void)
 {
+    /* Deregister this driver with the USB subsystem */
     usb_deregister(&pen_driver);
 }
  
@@ -70,4 +134,4 @@ module_exit(pen_exit);
  
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Ryan Spring");
-MODULE_DESCRIPTION("USB Pen Registration Driver");
+MODULE_DESCRIPTION("USB Pen Device Driver");
--- pen_register.mod.c
+++ pen_register.mod.c
@@ -20,7 +20,13 @@ __attribute__((section("__versions"))) = {
 	{ 0xa3ef6c50, "module_layout" },
 	{ 0x1837e804, "usb_deregister" },
 	{ 0xa1b4eb92, "usb_register_driver" },
+	{ 0xb9ce3a1a, "usb_register_dev" },
+	{ 0x4f8b5ddb, "_copy_to_user" },
 	{ 0x27e1a049, "printk" },
+	{ 0x8c9813d5, "usb_bulk_msg" },
+	{ 0x4f6b400b, "_copy_from_user" },
+	{ 0xa1c76e0a, "_cond_resched" },
+	{ 0x88789e21, "usb_deregister_dev" },
 	{ 0xb4390f9a, "mcount" },
 };
 
@@ -32,4 +38,4 @@ __attribute__((section(".modinfo"))) =
 MODULE_ALIAS("usb:v0781p5406d*dc*dsc*dp*ic*isc*ip*");
 MODULE_ALIAS("usb:vABCDp1234d*dc*dsc*dp*ic*isc*ip*");
 
-MODULE_INFO(srcversion, "D068896F40C6B840C21163B");
+MODULE_INFO(srcversion, "BFF1C47BEB4341A6A47F7FE");
